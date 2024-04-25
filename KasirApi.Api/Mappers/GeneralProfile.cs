321cbe1259456ebb3aa8ff1da8a44c10f7ae48d3
Muhammad Farid <mfarid2121@gmail.com>
2024-04-25 11:33:02 +0700

    add mapping for transaction

--- KasirApi.Api/Mappers/GeneralProfile.cs
+++ KasirApi.Api/Mappers/GeneralProfile.cs
@@ -13,6 +13,26 @@ namespace KasirApi.Api.Mappers
                 .ReverseMap();
             CreateMap<UserAddDto, User>();
             CreateMap<UserUpdDto, User>();
+
+            CreateMap<Transaction, TransactionViewDto>()
+                .ForMember(
+                    d => d.MemberName, 
+                    conf => conf.MapFrom(e => e.MemberId == null ? "" : "Example"))
+                .ReverseMap();
+            CreateMap<Transaction, TransactionViewDetailDto>()
+                .ForMember(
+                    d => d.MemberName, 
+                    conf => conf.MapFrom(e => e.MemberId == null ? "" : "Example"))
+                .ForMember(d => d.Details, conf => conf.MapFrom(e => e.TransactionDetails))
+                .ReverseMap();
+            CreateMap<TransactionAddDto, Transaction>()
+                .ForMember(d => d.TransactionDetails, conf => conf.MapFrom(e => e.Details));
+
+            CreateMap<TransactionDetail, TransactionDetailViewDto>()
+                .ForMember(d => d.ProductName, conf => conf.MapFrom(e => e.Product.Name))
+                .ReverseMap();
+            CreateMap<TransactionDetailAddDto, TransactionDetail>();
+            CreateMap<TransactionDetailUpdDto, TransactionDetail>();
         }
     }
 }
