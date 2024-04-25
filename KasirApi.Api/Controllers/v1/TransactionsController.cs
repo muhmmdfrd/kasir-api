fb289476d5dcd8a6dc9076878c8a8221d441e1af
Muhammad Farid <mfarid2121@gmail.com>
2024-04-25 11:33:35 +0700

    add transaction

---
+++ KasirApi.Api/Controllers/v1/TransactionsController.cs
@@ -0,0 +1,42 @@
+﻿using KasirApi.Api.Commons;
+using KasirApi.Core.Helpers;
+using KasirApi.Core.Models.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KasirApi.Api.Controllers.v1;
+
+[ApiVersion("1.0")]
+[Route("v1/[controller]")]
+[ApiController]
+[Authorize]
+public class TransactionsController : FlozaApiController
+{
+    private readonly TransactionHelper _helper;
+
+    public TransactionsController(TransactionHelper helper)
+    {
+        _helper = helper;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var result = await _helper.GetListAsync();
+        return ApiOK(result);
+    }
+    
+    [HttpGet("{id:long}")]
+    public async Task<IActionResult> Find([FromRoute] int id)
+    {
+        var result = await _helper.FindAsync(id);
+        return ApiOK(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] TransactionAddDto request)
+    {
+        var result = await _helper.CreateAsync(request, CurrentUser);
+        return result == 0 ? ApiDataInvalid() : ApiOK();
+    }
+}
---
+++ KasirApi.Core/Services/TransactionService.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Flozacode.Exceptions;
+using Flozacode.Repository;
+using KasirApi.Core.Interfaces;
+using KasirApi.Core.Models.Services;
+using KasirApi.Repository.Contexts;
+using KasirApi.Repository.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace KasirApi.Core.Services;
+
+public class TransactionService : ITransactionService
+{
+    private readonly IFlozaRepo<Transaction, AppDbContext> _repo;
+    private readonly IMapper _mapper;
+
+    public TransactionService(IFlozaRepo<Transaction, AppDbContext> repo, IMapper mapper)
+    {
+        _repo = repo;
+        _mapper = mapper;
+    }
+
+    public async Task<int> CreateAsync(TransactionAddDto value)
+    {
+        var entity = _mapper.Map<Transaction>(value);
+        return await _repo.AddAsync(entity);
+    }
+
+    public async Task<List<TransactionViewDto>> GetListAsync()
+    {
+        var result = _repo.AsQueryable
+            .ProjectTo<TransactionViewDto>(_mapper.ConfigurationProvider)
+            .ToList();
+        
+        return await Task.FromResult(result);
+    }
+
+    public async Task<TransactionViewDetailDto> FindAsync(int id)
+    {
+        var result = _repo.AsQueryable
+            .Include(q => q.TransactionDetails)
+            .AsSplitQuery()
+            .ProjectTo<TransactionViewDetailDto>(_mapper.ConfigurationProvider).
+            FirstOrDefault(u => u.Id == id);
+
+        if (result == null)
+        {
+            throw new RecordNotFoundException("Transaction not found.");
+        }
+        
+        return await Task.FromResult(result);
+    }
+}
